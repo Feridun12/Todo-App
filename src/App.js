3f447becc067e5ea1f451e4f474d7212315c3a87
Feridun Canselen <canselenferidun@gmail.com>
2022-02-13 17:26:44 +0200

    app finished and works excellent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+// importing components from now on
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  // STATES
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+  
+  // run once when the app starts
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  //USE EFFECT
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+  }, [todos, status]);
+
+  // FUNCTIONS
+
+  // saving to local so refresh does not make todo lis disappear
+  const saveLocalTodos = () => {
+   localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
+     let todoLocal = JSON.parse(localStorage.getItem("todos"));
+     setTodos(todoLocal);
+    }
+  };
+  
+const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Ferry's Todo List </h1>
       </header>
+      <Form
+        inputText={inputText}
+        setInputText={setInputText}
+        todos={todos}
+        setTodos={setTodos}
+        setStatus={setStatus}
+      />
+      <TodoList
+        filteredTodos={filteredTodos}
+        setTodos={setTodos}
+        todos={todos}
+      />
     </div>
   );
 }
